de877530eb1776191a06448ead1dfbfbe0039ce8
user19870 <87924678+user19870@users.noreply.github.com>
2025-10-21 20:13:43 +0800

    Implement window creation with custom loop

---
+++ example/detailed/create window with own loop.cpp
@@ -0,0 +1,13 @@
+#include <easywingui.h>
+
+ using namespace easywingui;easyw c;
+    
+ int main(){ 
+ 
+ 	c.creatw(L"title",1920,1200);  
+	   while(1){
+	   	c.runMessage(nullptr);Sleep(1);
+	   }
+ 
+ 	return 0;
+ } 
