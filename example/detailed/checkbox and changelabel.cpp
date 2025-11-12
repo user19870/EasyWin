6dd6fc9b54bf9470268b873074dd8c8f4bb6fb74
user19870 <87924678+user19870@users.noreply.github.com>
2025-11-12 16:33:28 +0800

    Add checkbox with label change functionality

---
+++ example/detailed/checkbox and changelabel.cpp
@@ -0,0 +1,15 @@
+#include <easywingui.h>
+using namespace easywingui;easyw c;
+bool checked=0;
+void check(){
+   c.changelabel("1", (!checked)? L"1":L"0"); checked=!checked;
+}
+int main(){
+    c.creatw(L"checkbox",400,300);c.label("1",L"0",200,100,100,30,"center");
+    checked=c.checkbox(L"checkbox",200,200,100,30,check); 
+    c.running=1;
+    while(c.running){
+    	c.runMessage(nullptr);Sleep(16);
+	}
+
+}
