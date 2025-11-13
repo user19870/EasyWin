fd55e6cc9b41e1864f1e66fcf87c5aba4ba76050
user19870 <87924678+user19870@users.noreply.github.com>
2025-11-13 21:29:02 +0800

    Update label creation with dynamic IDs

--- example/detailed/label and release.cpp
+++ example/detailed/label and release.cpp
@@ -10,9 +10,9 @@
  
  	c.creatw(L"hellowinTitle",400,400);c.button(L"textinbutton",150,500,100,100,c1);c.inputbox("id1",500,150,100,100,nullptr);
 	 c.release();
-     c.label(L"left",100,100,100,30,"left");
-			    c.label(L"center",100,200,100,30,"center");
-			     c.label(L"right",100,300,100,30,"right");
+     c.label("id1",L"left",100,100,100,30,"left");
+			    c.label("id2",L"center",100,200,100,30,"center");
+			     c.label("id3",L"right",100,300,100,30,"right");
 	  while(1){
 	  	c.runMessage(nullptr);Sleep(1);
 	  } 
