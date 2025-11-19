70798370b786ff511f85b6d2cb690b5017d8263e
user19870 <87924678+user19870@users.noreply.github.com>
2025-11-19 19:22:08 +0800

    Add radio button functionality in radiobutton.cpp
    Implement a simple GUI with radio buttons and labels.

---
+++ example/simple/radiobutton.cpp
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+  #include "easywingui.h"
+
+ using namespace easywingui;easyw c; bool r[6];
+   void run(){ 
+     if(c.radiostate(1)) c.changelabel("g1",L"Group 1: Selected");
+	  if(c.radiostate(2)) c.changelabel("g2",L"Group 2: Selected");
+}
+ int main(){ 
+ 	c.creatw(L" ",400,300);
+	 	c.label("g1",L"Group 1:",0,70,200,30,"center");
+	c.label("g2",L"Group 2:",0,160,200,30,"center");
+	   for(int i=0;i<6;i++){
+	    r[i]=(i<3)?c.radiobutton(L"g1",100+(i*100),100,100,30,1,nullptr):c.radiobutton(L"g2",100+(i-3)*100,130,100,30,2,nullptr); 
+	   }
+	   c.button(L"check",200,160,50,50,run);
+	 
+    c.loopAndStopProcessing();
+ 	return 0;
+ }
+ 
